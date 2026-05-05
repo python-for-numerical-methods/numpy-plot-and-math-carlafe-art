1fa11f2a438f7ee5197279178e6eaf84d267c98a
carlafe-art <carlafe@ac.sce.ac.il>
2026-05-05 13:30:42 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -17,7 +17,13 @@ def normalized_array(data):
     data = np.array(data)
     
     # --- כיתבו את הקוד שלכם כאן ---
-    pass
+    min_val = np.min(arr)
+    max_val = np.max(arr)
+
+    if max_val == min_val:
+        return np.zeros_like(arr, dtype=float)
+
+    return (arr - min_val) / (max_val - min_val)
     # חשוב לזכור להחליף את pass ב- return
 
 if __name__ == "__main__":
